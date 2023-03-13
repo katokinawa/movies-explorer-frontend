706969835303f6fa24cdc57a0f50fc77e5f7453c
Daniil Belogur <katokinawa@icloud.com>
2023-03-13 21:45:47 +0300

    feat: portfolio add links

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,19 +6,34 @@ function Portfolio() {
       <h2 className="title portfolio__main-title">Портфолио</h2>
       <div className="portfolio__text-wrapper">
         <p className="subtitle portfolio__subtitle">Статичный сайт</p>
-        <div className="strelka"></div>
+        <a
+          className="strelka"
+          href="https://katokinawa.github.io/Tinkoff-test/"
+          rel="noreferrer"
+          target="_blank"
+        ></a>
       </div>
       <div className="portfolio__undertitle-line"></div>
       <div className="portfolio__text-wrapper">
         <p className="subtitle portfolio__subtitle">Адаптивный сайт</p>
-        <div className="strelka"></div>
+        <a
+          className="strelka"
+          href="https://katokinawa.github.io/russian-travel/"
+          rel="noreferrer"
+          target="_blank"
+        ></a>
       </div>
       <div className="portfolio__undertitle-line"></div>
       <div className="portfolio__text-wrapper">
         <p className="subtitle portfolio__subtitle">
           Одностраничное приложение
         </p>
-        <div className="strelka"></div>
+        <a
+          className="strelka"
+          href="https://katokinawa.github.io/mesto/"
+          rel="noreferrer"
+          target="_blank"
+        ></a>
       </div>
       <div className="portfolio__undertitle-line"></div>
     </section>
