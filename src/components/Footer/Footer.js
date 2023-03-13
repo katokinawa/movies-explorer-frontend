7835a5d8f0d040f662c097e54055c5f7c5c9776e
Daniil Belogur <katokinawa@icloud.com>
2023-03-13 23:18:42 +0300

    fix: add footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,21 +2,39 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <section className="footer">
+    <footer className="footer">
       <h2 className="title footer__main-title">
         Учебный проект Яндекс.Практикум х BeatFilm.
       </h2>
       <div className="footer__undertitle-line"></div>
       <nav>
         <ul className="footer__links">
-          <li className="subtitle footer__subtitle">Яндекс.Практикум</li>
-          <li className="subtitle footer__subtitle">Github</li>
+          <li className="subtitle footer__subtitle">
+            <a
+              className="footer__link"
+              href="https://practicum.yandex.ru/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className="subtitle footer__subtitle">
+            <a
+              className="footer__link"
+              href="https://github.com/katokinawa"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
         </ul>
         <p className="subtitle footer__subtitle footer__copyright">
           © 2022. Даниил Белогур
         </p>
       </nav>
-    </section>
+    </footer>
   );
 }
 
