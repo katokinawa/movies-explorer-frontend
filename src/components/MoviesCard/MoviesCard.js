b71025383f30dddd1bd0b5f1bed0fa781d8f5d76
Daniil Belogur <katokinawa@icloud.com>
2023-03-19 21:27:34 +0300

    feat: img optimize

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -5,9 +5,9 @@ function MoviesCard() {
     <article className="movies-card">
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/33-slova-o-dizaine.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="33 слова о дизайне"
           className="movies-card__image"
         ></img>
         <h2 className="title movies-card__title">33 слова о дизайне</h2>
@@ -19,12 +19,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/kinoalmanah-100-let-dizaina.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="Киноальманах «100 лет дизайна»"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">Киноальманах «100 лет дизайна»</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
@@ -33,12 +33,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/v-pogone-za-benksi.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="В погоне за Бенкси"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">В погоне за Бенкси</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
@@ -47,12 +47,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/baksiya-vzryv-realnosti.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="Баския: Взрыв реальности"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">Баския: Взрыв реальности</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
@@ -61,12 +61,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/beg-eto-svoboda.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="Бег это свобода"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">Бег это свобода</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
@@ -75,12 +75,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/knigotvorcy.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="Книготорговцы"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">Книготорговцы</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
@@ -89,12 +89,12 @@ function MoviesCard() {
       </li>
       <li className="movies-card__item">
         <img
-          src={require("../../images/jenzina-i-deti.png")}
+          src={require("../../images/kogda-ya-dymay-o-germanii-nochyy.png")}
           loading="lazy"
-          alt="Обложка: На изображении девушка-репортёр, которая облокачивается на машину и смотрит на детей напротив неё. Она и дети улыбаются, они сидят и разговаривают, девушка одной рукой схватилась за камеру, будто хочет сделать снимок."
+          alt="Когда я думаю о Германии ночью"
           className="movies-card__image"
         ></img>
-        <h2 className="title movies-card__title">33 слова о дизайне</h2>
+        <h2 className="title movies-card__title">Когда я думаю о Германии ночью</h2>
         <p className="subtitle movies-card__subtitle">1ч 42м</p>
         <button
           type="button"
