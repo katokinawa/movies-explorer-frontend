9cca2362d230273b068d2288eca1bdbfd845e98a
Daniil Belogur <katokinawa@icloud.com>
2023-03-22 21:04:19 +0300

    fix: portfolio__text-wrapper

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -4,38 +4,37 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="title portfolio__main-title">Портфолио</h2>
-      <div className="portfolio__text-wrapper">
-        <p className="subtitle portfolio__subtitle">Статичный сайт</p>
-        <a
-          className="strelka"
-          href="https://katokinawa.github.io/Tinkoff-test/"
-          rel="noreferrer"
-          target="_blank"
-        ></a>
-      </div>
-      <div className="portfolio__undertitle-line"></div>
-      <div className="portfolio__text-wrapper">
-        <p className="subtitle portfolio__subtitle">Адаптивный сайт</p>
-        <a
-          className="strelka"
-          href="https://katokinawa.github.io/russian-travel/"
-          rel="noreferrer"
-          target="_blank"
-        ></a>
-      </div>
-      <div className="portfolio__undertitle-line"></div>
-      <div className="portfolio__text-wrapper">
-        <p className="subtitle portfolio__subtitle">
-          Одностраничное приложение
-        </p>
-        <a
-          className="strelka"
-          href="https://katokinawa.github.io/mesto/"
-          rel="noreferrer"
-          target="_blank"
-        ></a>
-      </div>
-      <div className="portfolio__undertitle-line"></div>
+      <ul className="portfolio__list">
+        <li className="portfolio__text-wrapper">
+          <p className="subtitle portfolio__subtitle">Статичный сайт</p>
+          <a
+            className="strelka"
+            href="https://katokinawa.github.io/Tinkoff-test/"
+            rel="noreferrer"
+            target="_blank"
+          ></a>
+        </li>
+        <li className="portfolio__text-wrapper">
+          <p className="subtitle portfolio__subtitle">Адаптивный сайт</p>
+          <a
+            className="strelka"
+            href="https://katokinawa.github.io/russian-travel/"
+            rel="noreferrer"
+            target="_blank"
+          ></a>
+        </li>
+        <li className="portfolio__text-wrapper">
+          <p className="subtitle portfolio__subtitle">
+            Одностраничное приложение
+          </p>
+          <a
+            className="strelka"
+            href="https://katokinawa.github.io/mesto/"
+            rel="noreferrer"
+            target="_blank"
+          ></a>
+        </li>
+      </ul>
     </section>
   );
 }
